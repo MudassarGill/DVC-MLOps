0e36c9c3d21a8901d2f3ab09fd1f3f54bd1b9ba2
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-02-11 00:04:35 -0800

    add dvc data

--- mycode.py
+++ mycode.py
@@ -7,8 +7,26 @@ data = {
     "units_sold": [10,5,8,7,12,6],
     "price_per_unit": [2,1,1.5,2,1,1.5]
 }
+df=pd.DataFrame(data)
+df.loc[0,'units_sold']=100
+df.loc[1,'units_sold']=200
+df.loc[2,'units_sold']=300
+df.loc[3,'units_sold']=400
+df.loc[4,'units_sold']=500
+df.loc[5,'units_sold']=600
+df.loc[0,'product']='mango'
+df.loc[1,'product']='grapes'
+df.loc[2,'product']='papaya'
+df.loc[3,'product']='apple'
+df.loc[4,'product']='banana'
+df.loc[5,'product']='orange'
+df.loc[0,'price_per_unit']=20
+df.loc[1,'price_per_unit']=10
+df.loc[2,'price_per_unit']=15
+df.loc[3,'price_per_unit']=20
+df.loc[4,'price_per_unit']=10
+df.loc[5,'price_per_unit']=15
 
-df = pd.DataFrame(data)
 data_dir='data'
 os.makedirs(data_dir,exist_ok=True)
 print(f'Folder created at {data_dir}')
