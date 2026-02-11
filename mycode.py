effb1aa22d31ef0b2b47a5def4ce6e065f9f6a40
Mudassar Gill <mudassarhussain6533@gmail.com>
2026-02-10 23:16:09 -0800

    first commit before DVC

---
+++ mycode.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+
+data = {
+    "date": ["2026-01-01","2026-01-02","2026-01-03","2026-01-04","2026-01-05","2026-01-06"],
+    "product": ["apple","banana","orange","apple","banana","orange"],
+    "units_sold": [10,5,8,7,12,6],
+    "price_per_unit": [2,1,1.5,2,1,1.5]
+}
+
+df = pd.DataFrame(data)
+data_dir='Data'
+os.makedirs(data_dir,exist_ok=True)
+file_path=os.path.join(data_dir,'sales.csv')
+df.to_csv(file_path,index=False)
+print(f"Data saved to {file_path}")
